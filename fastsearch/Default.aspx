e1dbd3c9076d00569d95a9cdbb87bc22c0db0f7f
corpse56 <debarkader@gmail.com>
2016-04-22 17:42:38 +0300

    Сделано для одной базы

---
+++ fastsearch/Default.aspx
@@ -0,0 +1,123 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"  CodeFile="Default.aspx.cs" Inherits="_Default"   %>
+
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="asp" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href = "css/my.css" type = "text/css" rel = "stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <asp:ToolkitScriptManager ID="ToolkitScriptManager1" runat="server">
+        </asp:ToolkitScriptManager>
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+    <ContentTemplate>
+    <div style="text-align:center; font-size:50px; padding:5px 50px 50px 50px;">
+    Единый поиск по всем каталогам <br/> Библиотеки Иностранной Литературы
+    </div><br />
+    <div style="text-align:center">
+        <asp:TextBox ID="TextBox1" runat="server" Width = "700px" Font-Size = "30px" ></asp:TextBox> &nbsp
+        <asp:Button ID="Button1" runat="server" Text="Найти" Font-Size = "23px" 
+            onclick="Button1_Click" />
+    </div>
+
+    
+    <div >
+    <br /><br />
+    <asp:Panel id = "MainSearchResults" runat="server" style="height:40px; width:100%; padding::10px 0px 10px 0px;background-color:#000000;">
+            <asp:Label ID="lMSR" runat="server" Text="" ForeColor="White" Font-Bold="true" Font-Size="30px"></asp:Label>
+            
+        </asp:Panel>
+    <br /><br /><br />
+        <asp:Panel id = "bjvvv_colapsediv" runat="server" style="height:60px; width:100%; padding:10px 0px 10px 0px;background-color:#98FB98;">
+            <asp:LinkButton ID="lbBJVVV" runat="server" Font-Size="23px">Результаты поиска в базе основного фонда. </asp:LinkButton> 
+            &nbsp&nbsp&nbsp&nbsp&nbsp<asp:Image ID="Image1" runat="server" />
+            <br />
+            <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+            
+        </asp:Panel>
+        
+        <asp:CollapsiblePanelExtender ID="CollapsiblePanelExtender1" runat="server" 
+            TargetControlID="pBJVVV"
+            CollapsedSize="0"
+            ExpandedSize="700"
+            Collapsed="True"
+            ExpandControlID="bjvvv_colapsediv"
+            CollapseControlID="bjvvv_colapsediv"
+            AutoCollapse="False"
+            AutoExpand="False"
+            ScrollContents="True"
+            TextLabelID="Label1"
+            CollapsedText="Показать..."
+            ExpandedText="Свернуть" 
+            ImageControlID="Image1"
+            ExpandedImage="~/images/collapse_blue.jpg"
+            CollapsedImage="~/images/expand_blue.jpg"> 
+        </asp:CollapsiblePanelExtender>
+        <asp:Panel ID="pBJVVV" runat="server"   Class = "HideHorizontalScrollBar" style = "overflow-x: hidden;">
+        
+         <asp:GridView ID="gvBJVVV" runat="server"  Width = "100%"
+        AutoGenerateColumns = "False"  BorderWidth="3px"
+         BorderStyle="Solid" BorderColor = "Black"
+            RowStyle-Wrap ="true"  Font-Size = "20px" CellPadding="3" 
+            AllowPaging = "true"  PageSize = "20" 
+        onpageindexchanging="gvBJVVV_PageIndexChanging"      >
+            
+<RowStyle Wrap="True"></RowStyle>
+            <Columns>
+                
+                <asp:BoundField HeaderText="Уровень">
+                <HeaderStyle BackColor="Silver" />
+                </asp:BoundField>
+
+                <asp:BoundField HeaderText="Автор" >
+                <HeaderStyle BackColor="Silver" />
+                </asp:BoundField>
+
+                <asp:TemplateField HeaderText="Заглавие" >
+                <HeaderStyle BackColor="Silver"  />
+                    <ItemTemplate>
+                        <asp:LinkButton ID="lbDetails" runat="server" CommandName="details" 
+                            Text='<%#Eval("TIT")%>'  CommandArgument='<%#Eval("IDMAIN")%>' >
+                        </asp:LinkButton>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                
+                <asp:BoundField HeaderText="Место издания" >
+                <HeaderStyle BackColor="Silver" />
+                </asp:BoundField>
+                
+                <asp:BoundField HeaderText="Издательство" >
+                <HeaderStyle BackColor="Silver" />
+                </asp:BoundField>
+
+                <asp:BoundField HeaderText="Дата издания" >
+                <HeaderStyle BackColor="Silver" />
+                </asp:BoundField>
+                <asp:BoundField HeaderText="E-lib" >
+                <HeaderStyle BackColor="Silver" />
+                </asp:BoundField>
+            </Columns>
+            
+        </asp:GridView>  
+    </asp:Panel>
+
+        <br />
+  </div>
+        <!--<div style="width:300px;height:auto; float:right; display:inline;padding:20px 20px 20px 20px; border-style:solid; border-width:thin; background-color:InactiveBorder; margin:20px 20px 20px 20px;" >
+    <div style="text-align:center;font-size:19px">
+        Памятка
+    </div>
+    <p>1. Знак "-" перед словом исключает результаты поиска с этим словом</p>
+    <p>2. Можно набирать часть слова</p>
+    </div>-->
+    </ContentTemplate>
+    </asp:UpdatePanel>
+    
+              
+    </form>
+</body>
+</html>
---
+++ fastsearch/Default.aspx.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Diagnostics;
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            bjvvv_colapsediv.Visible = false;
+            pBJVVV.Visible = false;
+            MainSearchResults.Visible = false;
+        }
+    }
+    int SRCount = 0;
+    double QueryTime = 0;
+    Stopwatch watch = new Stopwatch();
+
+    private void LoadDataBJVVV()
+    {
+        bjvvv_colapsediv.Visible = true;
+        MainSearchResults.Visible = true;
+
+        if (TextBox1.Text == "")
+        {
+            CollapsiblePanelExtender1.TextLabelID = "Ничего не найдено";
+            return;
+        }
+        MySqlDataAdapter da = new MySqlDataAdapter();
+        DataSet DS = new DataSet();
+        da.SelectCommand = new MySqlCommand();
+        var cs = ConfigurationManager.ConnectionStrings["SphinxConnectionString"].ConnectionString;
+        da.SelectCommand.Connection =  new MySqlConnection(cs);
+        da.SelectCommand.Parameters.AddWithValue("searchstr", TextBox1.Text);
+        da.SelectCommand.CommandText = "SELECT REMOVE_REPEATS((SELECT idmain,PLAIN FROM bjvvvidx where match(@searchstr) limit 0,10000), idmain, 0, 10000)";
+        watch.Start();
+        int i = da.Fill(DS, "pins");
+        watch.Stop();
+        SRCount += i;
+        string pins = "(";
+        foreach (DataRow r in DS.Tables["pins"].Rows)
+        {
+            pins += r["IDMAIN"].ToString() +",";
+        }
+        pins = pins.Remove(pins.Length - 1) + ")";
+        if (pins == ")") pins = "(null)";
+        SqlDataAdapter msda = new SqlDataAdapter();
+        msda.SelectCommand = new SqlCommand();
+        var mscs = ConfigurationManager.ConnectionStrings["msConnectionString"].ConnectionString;
+        msda.SelectCommand.Connection = new SqlConnection(mscs);
+
+        msda.SelectCommand.CommandText = "select 'not implemented' as LEVEL,avt.PLAIN as AVT,tit.PLAIN as TIT,mesto.PLAIN as MESTO,pub.PLAIN as IZD,god.PLAIN as GOD,'not implemented' as ELIB "+
+                                        ",A.IDMAIN IDMAIN   " +
+                                        " from BJVVV..DATAEXT A  "+
+                                        " left join BJVVV..DATAEXT Savt on A.IDMAIN = Savt.IDMAIN and Savt.MNFIELD = 700 and Savt.MSFIELD = '$a' " +
+                                        " left join BJVVV..DATAEXTPLAIN avt on avt.IDDATAEXT = Savt.ID " +
+                                        " left join BJVVV..DATAEXT Smesto on A.IDMAIN = Smesto.IDMAIN and Smesto.MNFIELD = 210 and Smesto.MSFIELD = '$a' " +
+                                        " left join BJVVV..DATAEXTPLAIN mesto on mesto.IDDATAEXT = Smesto.ID " +
+                                        " left join BJVVV..DATAEXT Spub on A.IDMAIN = Spub.IDMAIN and Spub.MNFIELD = 210 and Spub.MSFIELD = '$c' " +
+                                        " left join BJVVV..DATAEXTPLAIN pub on pub.IDDATAEXT = Spub.ID " +
+                                        " left join BJVVV..DATAEXT Sgod on A.IDMAIN = Sgod.IDMAIN and Sgod.MNFIELD = 2100 and Sgod.MSFIELD = '$d' " +
+                                        " left join BJVVV..DATAEXTPLAIN god on god.IDDATAEXT = Sgod.ID " +
+                                        " left join BJVVV..DATAEXTPLAIN tit on tit.IDDATAEXT = A.ID " +
+                                        " where A.MNFIELD = 200 and A.MSFIELD = '$a' and A.IDMAIN in " + pins;
+        int j = msda.Fill(DS, "t");
+        gvBJVVV.DataSource = DS.Tables["t"];
+
+        ((BoundField)gvBJVVV.Columns[0]).DataField = "LEVEL";
+        ((BoundField)gvBJVVV.Columns[1]).DataField = "AVT";
+        ((BoundField)gvBJVVV.Columns[3]).DataField = "MESTO";
+        ((BoundField)gvBJVVV.Columns[4]).DataField = "IZD";
+        ((BoundField)gvBJVVV.Columns[5]).DataField = "GOD";
+        ((BoundField)gvBJVVV.Columns[6]).DataField = "ELIB";
+
+        //((TemplateField)gvBJVVV.Columns[2]).DataField = "TIT";
+
+        gvBJVVV.DataBind();
+        if (gvBJVVV.Rows.Count == 0)
+        {
+            lbBJVVV.Text = "Результаты поиска в базе основного фонда. Найдено: " + i.ToString() + ".";
+            pBJVVV.Visible = false;
+        }
+        else
+        {
+            lbBJVVV.Text = "Результаты поиска в базе основного фонда. Найдено: " + i.ToString() + "...";
+            pBJVVV.Visible = true;
+        }
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        LoadDataBJVVV();
+        QueryTime = watch.Elapsed.TotalSeconds;
+        lMSR.Text = "По запросу '"+TextBox1.Text+"' найдено "+SRCount+" документов за "+QueryTime+" сек";
+        //pBJVVV.ScrollBars = ScrollBars.None;
+    }
+    protected void gvBJVVV_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        gvBJVVV.PageIndex = e.NewPageIndex;
+        LoadDataBJVVV();
+        gvBJVVV.DataBind();
+    }
+
+
+
+}
+        //<!--ImageControlID="Image1"
+        //ExpandedImage="~/ajax/images/collapse.jpg"
+        //CollapsedImage="~/ajax/images/expand.jpg"-->
